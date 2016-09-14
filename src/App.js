891858917a653b23396aae950b459bff817dfc32
mountainstar <chriswirthdesign@gmail.com>
2016-09-13 19:51:28 -0600

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import { Router } from 'react-router'
 import logo from './logo.svg';
+import routes from './routes';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+const history = createBrowserHistory();
 import './App.css';
+
 
 class App extends Component {
   render() {
+
+
     return (
+
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+            <Router history={history}>
+                { routes }
+            </Router>        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
+
   }
+
 }
 
 export default App;
