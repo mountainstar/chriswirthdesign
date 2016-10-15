2c87088a553536acdc99d3deb55608d486773ec8
mountainstar <chriswirthdesign@gmail.com>
2016-10-15 09:18:11 -0600

    added hero img // fixed hero heading

--- src/home/index.js
+++ src/home/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import bg from '../img/blog.jpg';
 
 class Home extends Component {
+
+
     render(){
+        const style = {
+            backgroundImage: 'url(' + bg + ')',
+            backgroundSize: '100%'
+                };
         return (
             <div className="container">
-                <div className="App-header">
+                <div className="App-header" style={style}>
                     <h2>Home</h2>
                 </div>
                 <section>
