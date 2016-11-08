aabf054d536310050add0ca1dcfb2a41dffdfeba
mountainstar <chriswirthdesign@gmail.com>
2016-11-08 15:14:04 -0700

    trying Json

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Route, IndexRoute, Link } from 'react-router'
 import Home from './home'
 import About from './about'
-import Work from './work'
 import Contact from './contact'
 import NoMatch from './nomatch'
 import logo from './cwd_logo.png';
@@ -34,7 +33,6 @@ export default (
     <Route path="/" component={App}>
         <IndexRoute component={Home}/>
         <Route path="about" component={About}/>
-        <Route path="work" component={Work}/>
         <Route path="contact" component={Contact}/>
         <Route path="*" component={NoMatch}/>
     </Route>
